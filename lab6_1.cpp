b30603c7fd2fc68778b058467cad7a1327bd6a0f
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-11-26 15:51:30 +0700

    Lab6

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,40 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{   
+    int num;
+	int countEven = 0;
+	int countOdd = 0;
+	cout << "Enter an integer: ";
+    cin>>num;
+	
+	if(num!=0)
+	{
+		while(num != 0)
+		{
+			cout << "Enter an integer: ";
+    		cin>>num;
+    			if(num%2==0)
+    			{
+    				countEven += 1;
+				}
+				else if(num%2==1)
+				{
+					countOdd += 1;
+				}
+    	
+    		if(num == 0)
+    		{
+    			cout << "#Even numbers = "<<countEven<<endl;
+    			cout << "#Odd numbers = "<<countOdd;
+			}
+        }
+	}
+	else
+	{
+		cout << "#Even numbers = "<<countEven<<endl;
+    	cout << "#Odd numbers = "<<countOdd;
+	}
     return 0;
 }
